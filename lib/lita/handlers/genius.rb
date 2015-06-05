0463168dca3cb789f37fcbd3657e1decba786ef0
Tristan Chong <tristanchong@gmail.com>
2015-06-05 16:19:19 -0700

    lib: route and #search

--- lib/lita/handlers/genius.rb
+++ lib/lita/handlers/genius.rb
@@ -1,6 +1,24 @@
+require "rapgenius"
+
 module Lita
   module Handlers
     class Genius < Handler
+
+      route(/^genius\s+(.+)/i, :search, command:true,
+            help: {t("help.search_key") => t("help.search_value")})
+
+      def search(response)
+        query = response.matches.first.first
+        songs = RapGenius.search(query)
+        if !songs.empty?
+          song = songs.first
+          response.reply song.description.gsub(/\n+/, " ")
+          response.reply "#{song.artist.name} - #{song.title} | http://genius.com/songs/#{song.id}"
+        else
+          response.reply "No songs found for '#{query}'"
+        end
+      end
+
     end
 
     Lita.register_handler(Genius)
